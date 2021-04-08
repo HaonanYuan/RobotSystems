eb503ad1862c7c928b53d54cd6dd83722cee82b5
SherlockY <37371094+HaonanYuan@users.noreply.github.com>
2021-04-07 21:32:19 -0700

    Add files via upload

--- picarx_improved.py
+++ picarx_improved.py
@@ -211,6 +211,7 @@ def test():
 def maneuvering_a(cmd):
     # forward
     if cmd == 'forward':
+        set_dir_servo_angle(0)
         forward(50)
     # backward
     elif cmd == 'backward':
@@ -331,7 +332,7 @@ if __name__ == "__main__":
     print('task a: Forward and backward in straight lines or with different steering angles\n')
     print('task b: Parallel-parking left and right\n')
     print('task c: Three-point turning (K-turning) with initial turn to the left or right\n')
-    print('task d: Stop the car and back to initial state\n')
+    print('task d: Stop the car and back to initial state')
     print('task e: End the whole task')
     while run:
         cmd = input('Please choose a task(a/b/c/d/e): ')
