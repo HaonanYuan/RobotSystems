954b72cb90fad95889ae0926a0ece67eb11f5fe8
SherlockY <37371094+HaonanYuan@users.noreply.github.com>
2021-05-20 13:15:21 +0800

    Add files via upload

---
+++ ArmPi/Functions/Motion_class.py
@@ -0,0 +1,33 @@
+import ArmIK.Transform as AT
+import HiwonderSDK.Board as Board
+from ArmIK.ArmMoveIK import *
+import time
+
+
+class motion:
+
+    def __init__(self):
+        self.AK = ArmIK()
+        self.servo = 500
+        self.servo2 = 500
+
+    def gripper_controller(self, angle):
+        Board.setBusServoPulse(1, self.servo + angle, 500)  # open the gripper
+        time.sleep(1)
+
+    def palm_controller(self, world_X=None, world_Y=None, rotation_angle=None):
+        if world_X is None and world_Y is None and rotation_angle is None:
+            Board.setBusServoPulse(2, self.servo2, 500)
+        else:
+            # calculate the angle that gripper need to rotate
+            servo2_angle = AT.getAngle(world_X, world_Y, rotation_angle)
+            Board.setBusServoPulse(2, servo2_angle, 500)
+        time.sleep(1)
+
+    def arm_controller(self, world_X, world_Y, distance):
+        result = self.AK.setPitchRangeMoving((world_X, world_Y, distance), -90, -90, 0, 1000)  # raise hand
+        time.sleep(result[2] / 1000)
+
+
+if __name__ == '__main__':
+    pass
