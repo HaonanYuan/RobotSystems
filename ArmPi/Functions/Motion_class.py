6d82abcb9d545d4af494c341959101c19da2b1fa
SherlockY <37371094+HaonanYuan@users.noreply.github.com>
2021-06-03 01:20:26 +0800

    Add files via upload

--- ArmPi/Functions/Motion_class.py
+++ ArmPi/Functions/Motion_class.py
@@ -11,22 +11,42 @@ class motion:
         self.servo = 500
         self.servo2 = 500
 
-    def gripper_controller(self, angle):
+    def initMove(self):
+        # Move arm to initial position
+        Board.setBusServoPulse(1, self.servo - 50, 300)
+        Board.setBusServoPulse(2, 500, 500)
+        self.AK.setPitchRangeMoving((0, 10, 10), -30, -30, -90, 1500)
+
+    def setBuzzer(self, timer):
+        # Set the buzzer and time
+        Board.setBuzzer(0)
+        Board.setBuzzer(1)
+        time.sleep(timer)
+        Board.setBuzzer(0)
+
+    def gripper_controller(self, angle=0):
+        # Control the gripper to open or close
+        # angle is negative, open the gripper
+        # angle is positive, close the gripper
         Board.setBusServoPulse(1, self.servo + angle, 500)  # open the gripper
         time.sleep(1)
 
     def palm_controller(self, world_X=None, world_Y=None, rotation_angle=None):
+        # Control the palm
         if world_X is None and world_Y is None and rotation_angle is None:
             Board.setBusServoPulse(2, self.servo2, 500)
+        elif world_X is None and world_Y is None and rotation_angle is not None:
+            Board.setBusServoPulse(2, rotation_angle)
         else:
             # calculate the angle that gripper need to rotate
             servo2_angle = AT.getAngle(world_X, world_Y, rotation_angle)
             Board.setBusServoPulse(2, servo2_angle, 500)
         time.sleep(1)
 
-    def arm_controller(self, world_X, world_Y, distance):
-        result = self.AK.setPitchRangeMoving((world_X, world_Y, distance), -90, -90, 0, 1000)  # raise hand
+    def arm_controller(self, world_X, world_Y, distance, alpha, alpha1, alpha2, movetime=None):
+        result = self.AK.setPitchRangeMoving((world_X, world_Y, distance), alpha, alpha1, alpha2, movetime)  # raise hand
         time.sleep(result[2] / 1000)
+        return result
 
 
 if __name__ == '__main__':
